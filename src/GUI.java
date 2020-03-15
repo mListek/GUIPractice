dbe6a0510ab74ca4fecd9f8f6b49c2c0bf2818f7
Michał Listek <michal.listex@gmail.com>
2020-03-15 15:12:25 +0100

    Made simple button that increase conunt

---
+++ src/GUI.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+    public GUI() {
+
+        frame = new JFrame();
+
+        JButton button = new JButton("Click me");
+        button.addActionListener(this);
+
+        label = new JLabel("Number of clics: 0");
+
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Our GUI");
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
+}
